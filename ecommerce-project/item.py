9907890db4f232fb1b366a84e27a04340c0c0a01
Harishankar <harishankar0301@gmail.com>
2022-12-09 22:56:11 +0530

    add ecommerce project

---
+++ ecommerce-project/item.py
@@ -0,0 +1,27 @@
+
+class item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_price(self, price):
+        self.price = price
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+
+    def __str__(self):
+        return self.name + " " + str(self.price) + " " + str(self.quantity)
